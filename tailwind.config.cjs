a90b772572e23da2853c45f5e5065cd61564d058
WebRepair <108232179+WebRepair@users.noreply.github.com>
2023-04-14 20:57:14 +0200

    new world 1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,31 +1,8 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
-      },
-      textColor: {
-        default: "var(--color-text)",
-        offset: "var(--color-text-offset)",
-      },
-      backgroundColor: {
-        default: "var(--color-background)",
-        offset: "var(--color-background-offset)",
-      },
-      borderColor: {
-        default: "var(--color-border)",
-      },
-    },
-  },
-  corePlugins: {
-    fontSize: false,
-  },
-  plugins: [require("tailwindcss-fluid-type")],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
