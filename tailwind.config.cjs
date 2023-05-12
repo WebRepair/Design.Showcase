83f78bbdd8ae2f8636b9f96678e4a70f97153f9c
WebRepair <108232179+WebRepair@users.noreply.github.com>
2023-05-12 17:34:10 +0200

    a

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'six' : "url('/src/data/showcase/images/six.JPG')",
+				'six2' : "url('/src/data/showcase/images/six2.JPG')",
+			}
+		},
 	},
 	plugins: [],
 }
